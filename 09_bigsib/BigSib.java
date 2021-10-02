7bf95b756e5da358226b7c810f1135cafc16b415
Jacob Ng <62451851+jng20@users.noreply.github.com>
2021-10-02 11:39:41 -0400

    more changes

--- 09_bigsib/BigSib.java
+++ 09_bigsib/BigSib.java
@@ -3,18 +3,15 @@ Baked Potatoes Emily Ortiz Vansh Saboo
 Jacob Ng and Preguac
 APCS
 HW09 -- What Are BigSibs Good For?
-2021-09-30
+2021-10-02
 **/
 
 /**
 Discoveries
--when putting in a string for a parameter for a method, you have to put it in quotes. 
-refer to lines 35,36,37.
--2 files can have the same name and different content as long as they're in different directories.
+
 
 Unresolved questions
--in the parameter for the method greet, don't we need a name for parameter String?
--Were we supposed to use string concatenation?
+
 **/
 
 
