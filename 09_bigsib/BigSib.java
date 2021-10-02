5ef5a06c80bd6b0c5188cb511e4f33d4f8278a40
Jacob Ng <62451851+jng20@users.noreply.github.com>
2021-10-02 12:06:19 -0400

    updated notes

--- 09_bigsib/BigSib.java
+++ 09_bigsib/BigSib.java
@@ -1,5 +1,5 @@
 /**
-Baked Potatoes Emily Ortiz Vansh Saboo
+Baked Potatoes Emily Ortiz and Apple Juice, Vansh Saboo and Tiffany,
 Jacob Ng and Preguac
 APCS
 HW09 -- What Are BigSibs Good For?
@@ -8,10 +8,14 @@ HW09 -- What Are BigSibs Good For?
 
 /**
 Discoveries
-
+- You can compile a program without a main() method. 
+- You can call a method from another file in the same directory if it has been compiled
+which is how the BigSib.greet part works.
 
 Unresolved questions
-
+- Are you calling the method from the other file's .java or .class?
+- I feel like this ability is useful but I can't currently see how it would be used.
+Can't we just throw all the methods into one .java file?
 **/
 
 
