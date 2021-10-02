2554b3b4d372c64cb4499ae08d02135f48838454
jacob ng <jng20@stuy.edu>
2021-10-02 11:31:03 -0400

    add Greet.java

---
+++ 09_bigsib/Greet.java
@@ -0,0 +1,39 @@
+/**
+Jacob Ng
+APCS
+HW08 -- Refactor Freshi Zero
+2021-09-30
+**/
+
+/**
+Discoveries
+-when putting in a string for a parameter for a method, you have to put it in quotes. 
+refer to lines 35,36,37.
+-2 files can have the same name and different content as long as they're in different directories.
+
+Unresolved questions
+-in the parameter for the method greet, don't we need a name for parameter String?
+-Were we supposed to use string concatenation?
+**/
+
+
+public class Greet{
+
+	public static void greet(String a){
+		String name;
+		name = a;
+		System.out.println("Why, hello there, " + name + ". How do you do?");
+	}
+
+
+
+
+
+
+
+	public static void main(String[] args){
+		greet("Bob");
+		greet("Joe");
+		greet("Greg");
+	}
+}
