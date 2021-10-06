d2df8fdef530fae81236febf3da069bf50e297b0
jacob ng <jng20@stuy.edu>
2021-10-06 15:25:03 -0400

    hw

---
+++ 13_hatch/BigSib.java
@@ -0,0 +1,40 @@
+/**
+Jacob Ng and Preguac, Kartic and Krrish
+APCS
+HW13 -- Where do BigSibs Come From?
+2021-10-06
+
+**/
+
+/**
+DISCO
+-error: non-static variable SupMsg cannot be referenced from a static context
+ The same error we saw in class. Had to remove static from the header to compile.
+- For each instance of a class, the static variable would remain the same throughout all of them.
+- Static can't reference non static and vice versa
+-stsatic has to do with memory stuff
+QCC
+- When richard.setHelloMsg("Word up"); 
+  only freshman appeared aka the input for greet() method
+-String SupMsg;
+ SupMsg = "Word up "; would not compile
+**/
+
+
+public class BigSib{
+
+	 String SupMsg = "Word up ";
+	
+	public String greet(String name){
+	
+		return SupMsg + name;
+	}
+	
+	
+	
+	//public static void setHelloMsg(String msg){
+		
+	//}
+}
+
+
