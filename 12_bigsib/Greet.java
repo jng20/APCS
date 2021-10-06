310485843ac89aa62781daca5532f7a014e86fcb
Jacob Ng <62451851+jng20@users.noreply.github.com>
2021-10-05 20:38:47 -0400

    Update Greet.java

--- 12_bigsib/Greet.java
+++ 12_bigsib/Greet.java
@@ -27,20 +27,20 @@ but it would require global variables and more knowledge about object properties
 
 
 public class Greet {
-  public static void main( String[] args ) {
-    String greeting;
+    public static void main( String[] args ) {
+        String greeting;
 
-    BigSib richard = new BigSib();    // BigSib richard has the properties of BigSib.java
-    richard.setHelloMsg("Word up");	  // setHelloMsg is a method in BigSib.java
-                                      // setHelloMsg does not need to be a string
-                                      // setHelloMsg can be a function that already prints a string (bigsib9 hw)
+        BigSib richard = new BigSib();    // BigSib richard has the properties of BigSib.java
+        richard.setHelloMsg("Word up");	  // setHelloMsg is a method in BigSib.java
+                                          // setHelloMsg does not need to be a string
+                                          // setHelloMsg can be a function that already prints a string (bigsib9 hw)
 
-    greeting = richard.greet("freshman");  // richard.greet("freshman") must be a string (bigsib10hw)
+        greeting = richard.greet("freshman");  // richard.greet("freshman") must be a string (bigsib10hw)
     					                             // richard.greet("freshman") is really BigSib.greet("freshman");
     					                             // so greeting is really BigSib.greet("freshman");
-    System.out.println(greeting);
+        System.out.println(greeting);
 
-  } //end main()
+    } //end main()
 } //end Greet
 
 
