35f1375aaae6adb2ae3542b50980a8167305d1b2
jacob ng <jng20@stuy.edu>
2021-10-05 20:10:35 -0400

    test file

---
+++ 12_bigsib/Greet.java
@@ -0,0 +1,40 @@
+/**
+Jacob Ng and Preguac, Kartic
+APCS
+HW12 -- On Elder Individuality and the Elimination of Radio Fuzz
+2021-10-05
+
+**/
+
+/**
+DISCO
+
+
+QCC
+
+**/
+
+
+public class Greet {
+  public static void main( String[] args ) {
+    String greeting;
+
+    BigSib richard = new BigSib();    //BigSib richard has the properties of BigSib.java
+    richard.setHelloMsg("Word up");	// setHelloMsg is a method in BigSib.java
+
+    greeting = richard.greet("freshman");  // richard.greet("freshman") must be a string
+    					    // richard.greet("freshman") is really BigSib.greet("freshman");
+    					    //so greeting is really BigSib.greet("freshman");
+    System.out.println(greeting);
+
+  } //end main()
+} //end Greet
+
+
+/**
+Output
+Word up freshman
+Salutations Dr. Spaceman
+Hey ya Kong Fooey
+Sup mom
+**/
