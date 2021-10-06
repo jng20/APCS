60f52cb4b86c08faaca365a53644cf980186b5e8
jacob ng <jng20@stuy.edu>
2021-10-05 20:09:51 -0400

    test file

---
+++ 12_bigsib/BigSib.java
@@ -0,0 +1,34 @@
+/**
+Jacob Ng and Preguac, Kartic
+APCS
+HW12 -- On Elder Individuality and the Elimination of Radio Fuzz
+2021-10-05
+
+**/
+
+/**
+DISCO
+
+
+QCC
+
+**/
+
+
+public class BigSib{
+
+	public static String greet(String name){
+	
+		return Msg + name;
+	}
+	
+	
+	
+	public static String setHelloMsg(String msg){
+		
+		Msg = msg;
+		return msg;
+	}
+}
+
+
