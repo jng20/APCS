10d27495c4d385eaf2d12518c83282774c267dc7
Jacob Ng <62451851+jng20@users.noreply.github.com>
2021-10-05 20:17:42 -0400

    Update Greet.java

--- 12_bigsib/Greet.java
+++ 12_bigsib/Greet.java
@@ -20,11 +20,13 @@ public class Greet {
     String greeting;
 
     BigSib richard = new BigSib();    //BigSib richard has the properties of BigSib.java
-    richard.setHelloMsg("Word up");	// setHelloMsg is a method in BigSib.java
+    richard.setHelloMsg("Word up");	  // setHelloMsg is a method in BigSib.java
+                                      // setHelloMsg does not need to be a string
+                                      // setHelloMsg can be a function that already prints a string 
 
     greeting = richard.greet("freshman");  // richard.greet("freshman") must be a string
-    					    // richard.greet("freshman") is really BigSib.greet("freshman");
-    					    //so greeting is really BigSib.greet("freshman");
+    					                             // richard.greet("freshman") is really BigSib.greet("freshman");
+    					                             //so greeting is really BigSib.greet("freshman");
     System.out.println(greeting);
 
   } //end main()
