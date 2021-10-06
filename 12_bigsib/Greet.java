b27ec9b64820e49f4b9cf361dfd5f22d72466fe0
Jacob Ng <62451851+jng20@users.noreply.github.com>
2021-10-05 20:27:14 -0400

    Update Greet.java

--- 12_bigsib/Greet.java
+++ 12_bigsib/Greet.java
@@ -8,10 +8,15 @@ HW12 -- On Elder Individuality and the Elimination of Radio Fuzz
 
 /**
 DISCO
-
+-setHelloMsg does not need to be a string
+-setHelloMsg can be a function that already prints a string (bigsib9 hw)
 
 QCC
-
+-richard.greet("freshman") must be a string (bigsib10hw)
+-richard.greet("freshman") is really BigSib.greet("freshman");
+-so greeting is really BigSib.greet("freshman");
+-BigSib richard has the properties of BigSib.java
+-setHelloMsg is a method in BigSib.java
 **/
 
 
@@ -19,14 +24,14 @@ public class Greet {
   public static void main( String[] args ) {
     String greeting;
 
-    BigSib richard = new BigSib();    //BigSib richard has the properties of BigSib.java
+    BigSib richard = new BigSib();    // BigSib richard has the properties of BigSib.java
     richard.setHelloMsg("Word up");	  // setHelloMsg is a method in BigSib.java
                                       // setHelloMsg does not need to be a string
-                                      // setHelloMsg can be a function that already prints a string 
+                                      // setHelloMsg can be a function that already prints a string (bigsib9 hw)
 
-    greeting = richard.greet("freshman");  // richard.greet("freshman") must be a string
+    greeting = richard.greet("freshman");  // richard.greet("freshman") must be a string (bigsib10hw)
     					                             // richard.greet("freshman") is really BigSib.greet("freshman");
-    					                             //so greeting is really BigSib.greet("freshman");
+    					                             // so greeting is really BigSib.greet("freshman");
     System.out.println(greeting);
 
   } //end main()
