265ade86a94e65e3599a057286b98ec387421371
Jacob Ng <62451851+jng20@users.noreply.github.com>
2021-10-07 21:33:15 -0400

    Update BigSib.java

--- 14_overload/overcon/BigSib.java
+++ 14_overload/overcon/BigSib.java
@@ -1,33 +1,40 @@
 /**
-Jacob Ng and Preguac, Kartik and Krrish
+Team Richard
+Jacob Ng and Preguac, Kartik and Krrish, Vansh and Tiffany
 APCS
 HW14 -- Customize Your Creation
 2021-10-07
-
+Thanks to the dojo for showing me what a constructor was
 **/
 
 /**
 DISCO
-
+-I discovered what a constructor actually was (I had only been making instance variables)
+-I discovered how an overloaded constructor works (a constructor that takes args)
+-I discovered contructors are methods
 QCC
-
+-I don't get how to use both a constructor and overloaded constructor together
+-The instance variable should be private
+-I don't think I did this hw right. My team also didn't know.
+-How do we give each Instance their own personal msg without an input? Is it possible without 
+conditionals?
+-Is there a way to get the name of the new Instance made? 
 **/
 
 
 public class BigSib{
 
-	
-	
-	public String greet(String name){
-	
-		return Msg + " " + name;
-	}
-	
-	
-	
-	//public static void setHelloMsg(String msg){
-		
-	//}
-}
+    private String msg;
 
+    public BigSib(String supMsg){
+        msg = supMsg;
+    }
+
+    public String greet(String name){
+
+	return msg + " " + name;
+    }
+
+
+}
 
