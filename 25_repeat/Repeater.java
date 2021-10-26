9fa0623e6ae3a1d011423e6b91b711eb06b9b5e4
jacob ng <jng20@stuy.edu>
2021-10-26 16:52:10 -0400

    stuff

--- 25_repeat/Repeater.java
+++ 25_repeat/Repeater.java
@@ -12,10 +12,25 @@ public class Repeater{
 		
 	}
 	
+	public static String fenceW(int numPosts){
+		String fence = "|";
+		while(numPosts > 1){
+			fence += "--|";
+			numPosts --;
+		}
+		return fence;
+		
+	}
+	
 	public static void main(String[] args){
 		System.out.println(fenceR(3));
 		System.out.println(fenceR(1));
 		System.out.println(fenceR(2));
 		System.out.println(fenceR(4));
+		
+		System.out.println(fenceW(3));
+		System.out.println(fenceW(1));
+		System.out.println(fenceW(2));
+		System.out.println(fenceW(4));
 	}
 }
