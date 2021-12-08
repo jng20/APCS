0fffe6103c0448cf4771fd6357f30ac94dc51672
jacob ng <jng20@stuy.edu>
2021-12-08 17:04:12 -0500

    im confused

---
+++ 45_suparr/ListInt.java
@@ -0,0 +1,19 @@
+/*****************************************************
+  * Team Turtle Hats
+  * Jacob Ng Kevin Cheng Hamim Seam
+  * APCS
+  * HW45 -- Array of Titanium
+  * 2021-12-07
+  * Time spent: 1hrs
+  *****************************************************/
+
+
+public interface ListInt{
+
+    public boolean add();
+    public int get();
+    public int set();
+    public int size();
+
+
+}
