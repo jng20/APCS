b262ccf2743620240bbdf3b3c1f34d9b0717afa6
Jacob Ng <jng20@stuy.edu>
2022-03-08 19:52:14 -0500

    need to find best and worst case. Need to fix last 2 cases

--- 72_fastselect/FastSelect.java
+++ 72_fastselect/FastSelect.java
@@ -5,35 +5,44 @@
 
 /*
 DISCO
-
+We have to realize that the yth element is the y -1 index.
+When you run partition it changes the original array because of swap
+Everytime you call partition, even in an if statement, the array will change so you have to call it right at the beginning.
 QCC
 This is similar to binary search but we can use an unsorted list.
+If java is pass by value, how come after fastSelect is run, the original array is coming out sorted. *answer: swap is changing the array*
 
 It doesn't matter what we use as the pivot because the S value(place of the partition) is what we care about.
-To stay consistent we'll use the last index as the pivot whenever we use partition. 
+To stay consistent we'll use the last index as the pivot whenever we use partition.
 
-ALGO 
+ALGO
 Run partition on the array with the last index being the pivot. If S (place of the partition) = y then return the pivot value. Else:
     - If S is less than y, we know that the values on the left of s are also smaller than y so we need to look at the values to the right of s.
         -Run partition again but change the range from s to the end and subtract s from y.
     - If S is more than y, we know that the values on the right of s are also greater than y so we need to look at the values to the left of s.
-        - Run partition again but change the range from beginning to s
-BEST CASE SCENARIO: 
-WORST CASE SCENARIO: 
+        - Run partition again but change the range from beginning to s-1
+
+BEST CASE SCENARIO: After first partition the yth value is found. In our case the yth value is last item.
+WORST CASE SCENARIO: After n partitions the yth value is found. In our case,after every partition, the yth value always goes at least 2 infront of the last item?
 Include test cases in main() method illustrating each best- and worst-case scenario.
 */
 
 public class FastSelect{
-    
+
      private static int[] foo = {7,1,5,12,3};
-     
+     private static int[] temper = {1,3,5,12,7};
+     private static int[] foo1 = {7,1,5,12,3};
+     private static int[] foo2 = {7,1,5,12,3};
+     private static int[] foo3 = {7,1,5,12,3};
+     private static int[] foo4 = {7,1,5,12,3};
+
      public static void swap( int x, int y, int[] o ){
         int tmp = o[x];
         o[x] = o[y];
         o[y] = tmp;
      }
 
-    //print input array 
+    //print input array
     public static void printArr( int[] a ){
         for ( int o : a )
             System.out.print( o + " " );
@@ -50,7 +59,7 @@ public class FastSelect{
             swap( i, swapPos, d );
         }
     }
-    
+
   //return int array of size s, with each element fr range [0,maxVal)
     public static int[] buildArray( int s, int maxVal ){
         int[] retArr = new int[s];
@@ -58,45 +67,67 @@ public class FastSelect{
             retArr[i] = (int)( maxVal * Math.random() );
         return retArr;
     }
-    
+
     public static int partition( int arr[], int first, int end, int pivot){
-	    //printArr(arr);
+	    printArr(arr);
 	    int v = arr[pivot];
-		swap(pivot, end, arr);
-		int s = first;  //where the pivot will end up at the end of function aka where the array is split
-		
-		for(int i = 0; i < end; i++){
+		  swap(pivot, end, arr);
+		  int s = first;  //where the pivot will end up at the end of function aka where the array is split
+
+		for(int i = first; i < end-1; i++){               //i HAS TO EQUAL FIRST NOT 0
 			if(arr[i] < v){
 				swap(i, s, arr);
 				s++;
 			}
 		}
 		swap(end, s, arr);
-		//printArr(arr);
+		printArr(arr);
 		return s;
     }
-    
+
     public static int fastSelect(int y, int array[], int first, int end, int pivot){
         int s = 0;
-        int length = array.length;
-        
-        if(partition(array, first, end, pivot) < (y - 1)){     // y-1 because the yth element is the y - 1 index.
-            s = partition(array, first, end, pivot);
-            return fastSelect(y-s,array, s+1, length, length);
-            
-        }else if(partition(array, first, end, pivot) > (y - 1) ){    
-            s = partition(array, first, end, pivot);
+        //int length = array.length - 1;
+        printArr(array);
+        //System.out.println();
+        int piv_Value = array[pivot];
+        s = partition(array, first, end, pivot);
+
+        // if(partition(array, first, end, pivot) < (y - 1)){            // y-1 because the yth element is the y - 1 index.
+        //     s = partition(array, first, end, pivot);
+        //     return fastSelect(y-s,array, s+1, length, length);
+        //
+        // }else if(partition(array, first, end, pivot) > (y - 1) ){
+        //     s = partition(array, first, end, pivot);
+        //     return fastSelect(y, array, first, s-1, s-1);
+        if(s == (y-1)){
+            return piv_Value;
+        }else if(s < (y -1)){
+            return fastSelect(y-s,array, s, end, end);
+        }else{                                                          //if(s > (y - 1) )
             return fastSelect(y, array, first, s-1, s-1);
-            
-        }else{
-            return array[pivot];
         }
     }
-    
+
     public static void main(String[] args){
-		//partition(foo, 0, 4, 2);
-		fastSelect(3, foo, 0, 4, 4);
+	//	System.out.println(partition(foo, 0, 4, 4));
+  //  System.out.println(partition(temper, 2, 4, 4));
+
+    //System.out.println();
+		//System.out.println(fastSelect(3, foo, 0, 4, 2));
+
+    System.out.println(fastSelect(1, foo, 0, 4, 4));
+    //printArr(foo);
+    System.out.println();
+    System.out.println(fastSelect(2, foo1, 0, 4, 4));
+    System.out.println();
+    System.out.println(fastSelect(3, foo2, 0, 4, 4));
+    System.out.println();
+    System.out.println(fastSelect(4, foo3, 0, 4, 4));         // doesn't work
+    System.out.println();
+    System.out.println(fastSelect(5, foo4, 0, 4, 4));         // doesn't work
+
+
+
 	}
 }
-
-
